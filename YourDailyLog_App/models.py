e6ca691ecd791cd19c9d1417f45922c8df66e608
Zad-100 <akash2001parida@gmail.com>
2021-11-14 21:41:28 +0530

    Made several changes. Mainly created the classes Topic and Entry in the models.py file. Also created a superuser (admin) and registered the first data (by admin only) that can be used to work on with and debug the program or project.

--- YourDailyLog_App/models.py
+++ YourDailyLog_App/models.py
@@ -8,3 +8,16 @@ class Topic(models.Model):
     def __str__(self):
         """Returns the string representation of the model"""
         return self.topicName
+
+class Entry(models.Model):
+    """Something specific about a topic (an Entry about a topic, that is"""
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    entryText = models.TextField()
+    dateAndTimeAdded = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = "Entries"
+
+    def __str__(self):
+        """Return a string representation of the model"""
+        return f"{self.entryText[: 50]}..."
