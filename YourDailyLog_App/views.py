507dd62d3ae95ab52fa4661ef7e486eed2d17cbb
Zad-100 <akash2001parida@gmail.com>
2021-11-15 23:42:42 +0530

    Created a view for the home page that renders the index.html file

--- YourDailyLog_App/views.py
+++ YourDailyLog_App/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    """The homepage for Your Daily Log"""
+    return render(request, "yourdailylog_app/index.html")
