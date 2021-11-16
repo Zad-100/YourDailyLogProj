31055eae7b034c69c356054902c41f2677b75113
Zad-100 <akash2001parida@gmail.com>
2021-11-16 21:23:02 +0530

    Added a 'Topics' page which shows all the topics by a user. Each topic is a link to the entries page which shows all the entries of that particular topic in chronological order.

--- YourDailyLog_App/urls.py
+++ YourDailyLog_App/urls.py
@@ -8,4 +8,10 @@ app_name = "YourDailyLog_App"
 urlpatterns = [
     # Homepage
     path("", views.index, name='index'),
+
+    # Topics page - shows all the topics for a user
+    path('topics/', views.topics, name="topics"),
+
+    # Individual topic page which shows the entries made by user
+    path("topics/<int:topicID>/", views.topic, name='topic'),
 ]
--- YourDailyLog_App/views.py
+++ YourDailyLog_App/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render
 
+from .models import Topic
+
 def index(request):
     """The homepage for Your Daily Log"""
     return render(request, "yourdailylog_app/index.html")
+
+def topics(request):
+    """The topics page that shows all topics entered by a user"""
+    topicNames = Topic.objects.order_by('dateAndTime')
+    context = {'topicNames': topicNames}
+
+    return render(request, "yourdailylog_app/topics.html", context)
+
+def topic(request, topicID):
+    """
+       Show all the entries with their time stamp for a particular topic.
+       The entries are shown in reverse chronological order.
+    """
+    topicName = Topic.objects.get(id=topicID)
+    entries = topicName.entry_set.order_by("-dateAndTimeAdded")
+    context = {'topicName': topicName, 'entries': entries}
+
+    return render(request, 'yourdailylog_app/topic.html', context)
