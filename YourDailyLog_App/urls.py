8fa52bd9883ceb4805df541dcc8036f76fce4bfa
Zad-100 <akash2001parida@gmail.com>
2021-11-18 21:58:54 +0530

    Created a page for adding new topics that has a 'form' which prompts the user to add a new topic name and has a submit button. After processing the 'POST' request, the page redirects to the 'Topics' page. Also added the URL to the 'new topics' page to the 'Topics' page after the list of the topics end.

--- YourDailyLog_App/urls.py
+++ YourDailyLog_App/urls.py
@@ -7,11 +7,20 @@ from . import views
 app_name = "YourDailyLog_App"
 urlpatterns = [
     # Homepage
+    # The URL route is left blank as it defaults to the base django page
+    # The requested URL is routed to its respective view 
+    # The second argument specifies which views file to call in views.py
+    # The third argument assigns a name to this URL pattern so that we can refer
+    # to it in other programs
     path("", views.index, name='index'),
 
     # Topics page - shows all the topics for a user
     path('topics/', views.topics, name="topics"),
 
     # Individual topic page which shows the entries made by user
+    # Uses the topic's ID assigned automatically to route to that page
     path("topics/<int:topicID>/", views.topic, name='topic'),
+
+    # Page for adding a new topic
+    path("new_topic/", views.new_topic, name="new_topic"),
 ]
--- YourDailyLog_App/views.py
+++ YourDailyLog_App/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .models import Topic
+
+from .forms import TopicForm # programmer defined
 
 def index(request):
     """The homepage for Your Daily Log"""
@@ -9,7 +11,8 @@ def index(request):
 def topics(request):
     """The topics page that shows all topics entered by a user"""
     topicNames = Topic.objects.order_by('dateAndTime')
-    context = {'topicNames': topicNames}
+    context = {'topicNames': topicNames} # context is to be passed to the
+                                         # template so that it can work on it
 
     return render(request, "yourdailylog_app/topics.html", context)
 
@@ -19,7 +22,40 @@ def topic(request, topicID):
        The entries are shown in reverse chronological order.
     """
     topicName = Topic.objects.get(id=topicID)
+
+    # the '-' sign before dateAndTimeAdded attribute is used to order in reverse
+    # order, that is in descending order
     entries = topicName.entry_set.order_by("-dateAndTimeAdded")
     context = {'topicName': topicName, 'entries': entries}
 
     return render(request, 'yourdailylog_app/topic.html', context)
+
+def new_topic(request):
+    """
+        Adds a new topic. Also redirects the user back to the topics page
+        after the user has submitted the form with the new topic name
+    """
+
+    # 'POST' requests are made when the user requests to process a submitted
+    # form
+    # 'GET' requests are made when the request is to read information from the
+    # server
+    if request.method != 'POST': # that means the user requesting for a blank
+                                 # form
+                                 # 'GET' request (probably)
+        # No data submitted; create a blank form
+        form = TopicForm() # instance created of TopicForm
+    else:
+        # POST data submitted; process data
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            
+            # redirect() takes in the name of a view and redirect the user to
+            # that view
+            return redirect('YourDailyLog_App:topics')
+
+    # Display a blank or invalid form
+    context = {"form": form}
+
+    return render(request, 'yourdailylog_app/new_topic.html', context)
