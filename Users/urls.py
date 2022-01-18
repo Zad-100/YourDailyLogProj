b52960dec2203a2a7be9f3f66abbe5b620673a36
Zad-100 <akash2001parida@gmail.com>
2022-01-18 14:17:09 +0530

    Created the registration page. Wrote my own view function for registration and linked it to the register.html template. Kept the register link in base.html. Used Django's in-built UserCreationForm() class to create the form for registration in the view function. All working all good so far. Two new users also created to test the working so far.

--- Users/urls.py
+++ Users/urls.py
@@ -2,8 +2,13 @@
 
 from django.urls import path, include
 
+from . import views # to write programmer defined views function
+
 app_name = 'Users'
 urlpatterns = [
     # Include default authentication urls
     path("", include("django.contrib.auth.urls")),
+
+    # Registration page
+    path("register/", views.register, name="register"),
 ]
--- Users/views.py
+++ Users/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
+def register(request):
+    """Register a new user to the app"""
+    
+    if request.method != 'POST':
+        # Display blank registration form.
+        formForRegistration = UserCreationForm() # creating an instance
+    else:
+        # Process completed form
+        formForRegistration = UserCreationForm(data=request.POST)
+
+        if formForRegistration.is_valid():
+            newUser = formForRegistration.save()
+
+            # Log the user in and then return to the home page
+            login(request, newUser)
+
+            return(redirect('YourDailyLog_App:index'))
+
+    # Display a blank or invalid form
+    context = {"formForRegistration": formForRegistration}
+
+    return(render(request, "registration/register.html", context))
