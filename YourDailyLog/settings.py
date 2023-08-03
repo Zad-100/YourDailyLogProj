4b760d3ac1bf337b5cf9e6d60d23825914479c77
Zad-100 <akash2001parida@gmail.com>
2023-08-03 13:11:30 +0530

    Added Azure to the allowed hosts in the settings.py file for Azure Web App Deployment.

--- YourDailyLog/settings.py
+++ YourDailyLog/settings.py
@@ -27,7 +27,9 @@ SECRET_KEY = 'django-insecure-f#1(3kz8k&4=x0psmt1ez!yh^4gr2^gp4lyc+2(g%(!^^ek73q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "your-daily-log.azurewebsites.net",
+]
 
 
 # Application definition
